ede1594f4ea2ccb119968bf4e487b2bdea6c4503
bhollister14 <bhollister14@punahou.edu>
2014-02-03 14:23:02 -1000

    Creates a new file with custom name
    When run, with command “create new”, lets user name the file in the
    next line. Things to edit it to come later.

--- FileRead.java
+++ FileRead.java
@@ -5,29 +5,34 @@ import java.io.*;
 import java.util.Scanner;
 
 public class FileRead{
-   public static void main(String args[])throws IOException{
+  //public static void main(String args[])throws IOException{
+  public static void createNewFile(String fileName)throws IOException{
 //CREATE THE FILE
-      File file = new File("castList.txt");
-      // creates the file
-      file.createNewFile();
-      // creates a FileWriter Object
-      FileWriter writer = new FileWriter(file); 
-      // Writes the content to the file
-      Scanner kb = new Scanner(System.in);
+    File file = new File(fileName+".txt");
+    // creates the file
+    file.createNewFile();
+    // creates a FileWriter Object
+    FileWriter writer = new FileWriter(file); 
+    // Writes the content to the file
+  }
+  public void writeToFile(FileWriter writer)throws IOException{
 //WRITE TO THE FILE
-      if (kb.nextLine().equals("add person"))
-      {
-      writer.write(kb.nextLine()); 
+    Scanner keyb = new Scanner(System.in);
+    if (keyb.nextLine().equals("add person"))
+    {
+      writer.write(keyb.nextLine()); 
       writer.flush();
       writer.close();
-      }
+    }
+  }
+  public void readFile(File file)throws IOException{
 //READ THE FILE
-      //Creates a FileReader Object
-      FileReader fr = new FileReader(file); 
-      char [] a = new char[50];
-      fr.read(a); // reads the content to the array
-      for(char c : a)
-          System.out.print(c); //prints the characters one by one
-      fr.close();
-   }
+    //Creates a FileReader Object
+    FileReader fr = new FileReader(file); 
+    char [] a = new char[50];
+    fr.read(a); // reads the content to the array
+    for(char c : a)
+      System.out.print(c); //prints the characters one by one
+    fr.close();
+  }
 }
---
+++ VarietyShowManager.java
@@ -0,0 +1,18 @@
+//Brianna-Marie Hollister
+//Feb. 3, 2014
+//Variety Show Manager (File Writing Practice)
+
+import java.util.Scanner;
+import java.io.*;
+
+public class VarietyShowManager
+{
+  public static void main(String args[])throws IOException
+  {
+    Scanner kb = new Scanner(System.in);
+    if (kb.nextLine().equals("create new"))
+    {
+      FileRead.createNewFile(kb.nextLine());
+    }
+  }
+}

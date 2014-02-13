11a1ee2f41aba225ae4a1041fbd08470e03a34b0
bhollister14 <bhollister14@punahou.edu>
2014-02-12 23:00:50 -1000

    Reading and printing text written onto txt files
    JUST MIND THE .JAVA FILES (it wouldn’t let me commit otherwise?) The
    only difference between this and the last update is that this one
    can read text from the text file.  Right now I just have the text being
    printed onto the screen, but this allows me to move onto storing text…
    Credits for code from the internet can be found in the FileRead class.

--- VarietyShowManager.java
+++ VarietyShowManager.java
@@ -1,15 +1,30 @@
 //Brianna-Marie Hollister
-//Feb. 3, 2014
+//Feb. 5, 2014
 //Variety Show Manager (File Writing Practice)
 
 import java.util.Scanner;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class VarietyShowManager
 {
-  public static void main(String args[])//throws IOException
+  public static void main(String args[])throws IOException
   {
     Scanner kb = new Scanner(System.in);
-    VSstartup.isCorrectFile();
+    System.out.println("Please type the name of the file you wish to open.  If there is none, type 'none'.");
+    String fileName = new String(kb.nextLine());
+    if (fileName.equals("none"))
+    {
+      System.out.println("Enter file name:");
+      fileName = kb.nextLine();
+      File file = new File(fileName+".txt");
+      file.createNewFile();
+      FileWriter writer = new FileWriter(file);
+      VSstartup.writeToFile(writer);
+    }
+    VSstartup.isCorrectFile(fileName);
+    System.out.println(VSstartup.openFile(fileName));
   }
 }
